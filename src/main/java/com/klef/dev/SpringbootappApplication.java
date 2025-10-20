9c47f42bac144ec6c1975f313fc22d8ffa3d65e0
Lahari2335 <your_github_email@example.com>
2025-10-20 15:58:52 +0530

    updated

--- src/main/java/com/klef/dev/SpringbootappApplication.java
+++ src/main/java/com/klef/dev/SpringbootappApplication.java
@@ -2,13 +2,13 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootappApplication {
+public class SpringbootappApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootappApplication.class, args);
-		System.out.println("Backend is running");
-		}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootappApplication.class, args);
+        System.out.println("Backend is running on Tomcat port 2030...");
+    }
 }
